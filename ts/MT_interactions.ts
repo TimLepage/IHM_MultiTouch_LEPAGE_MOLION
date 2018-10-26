8220c0f1b7636adb83d29dc8a33372e60657fa07
Tim Lepage <timlepage9@gmail.com>
2018-10-26 12:03:32 +0200

    rotozoom ok

--- ts/MT_interactions.ts
+++ ts/MT_interactions.ts
@@ -1,105 +1,149 @@
 import { FSM } from "./FSM";
 import * as transfo from "./transfo";
 
-function multiTouch(element: HTMLElement) : void {
-    let pointerId_1 : number, Pt1_coord_element : SVGPoint, Pt1_coord_parent : SVGPoint,
-        pointerId_2 : number, Pt2_coord_element : SVGPoint, Pt2_coord_parent : SVGPoint,
-        originalMatrix : SVGMatrix,
-        getRelevantDataFromEvent = (evt : TouchEvent) : Touch => {
-            for(let i=0; i<evt.changedTouches.length; i++) {
+function multiTouch(element: HTMLElement): void {
+    let pointerId_1: number, Pt1_coord_element: SVGPoint, Pt1_coord_parent: SVGPoint,
+        pointerId_2: number, Pt2_coord_element: SVGPoint, Pt2_coord_parent: SVGPoint,
+        originalMatrix: SVGMatrix,
+        getRelevantDataFromEvent = (evt: TouchEvent): Touch => {
+            for (let i = 0; i < evt.changedTouches.length; i++) {
                 let touch = evt.changedTouches.item(i);
-                if(touch.identifier === pointerId_1 || touch.identifier === pointerId_2) {
+                if (touch.identifier === pointerId_1 || touch.identifier === pointerId_2) {
                     return touch;
                 }
             }
             return null;
         };
-    enum MT_STATES {Inactive, Translating, Rotozooming}
-    let fsm = FSM.parse<MT_STATES>( {
+    enum MT_STATES { Inactive, Translating, Rotozooming }
+    let fsm = FSM.parse<MT_STATES>({
         initialState: MT_STATES.Inactive,
         states: [MT_STATES.Inactive, MT_STATES.Translating, MT_STATES.Rotozooming],
-        transitions : [
-            { from: MT_STATES.Inactive, to: MT_STATES.Translating,
+        transitions: [
+            {
+                from: MT_STATES.Inactive, to: MT_STATES.Translating,
                 eventTargets: [element],
                 eventName: ["touchstart"],
                 useCapture: false,
-                action: (evt : TouchEvent) : boolean => {
-                    // To be completed
+                action: (evt: TouchEvent): boolean => {
+
+                    pointerId_1 = 0;
+                    pointerId_2 = 1;
+                    console.log(getRelevantDataFromEvent(evt));
+
+                    originalMatrix = transfo.getMatrixFromElement(element);
+
+                    // Multiplication of matrix point is done via matrixTransform (wtf?) : https://developer.mozilla.org/en-US/docs/Web/API/SVGPoint
+                    Pt1_coord_element = transfo.getPoint(getRelevantDataFromEvent(evt).pageX, getRelevantDataFromEvent(evt).pageY).matrixTransform(originalMatrix.inverse());
+                    Pt1_coord_parent = transfo.getPoint(getRelevantDataFromEvent(evt).pageX, getRelevantDataFromEvent(evt).pageY);
+
+                    console.log("inactive to translating done");
                     return true;
                 }
             },
-            { from: MT_STATES.Translating, to: MT_STATES.Translating,
+            {
+                from: MT_STATES.Translating, to: MT_STATES.Translating,
                 eventTargets: [document],
                 eventName: ["touchmove"],
                 useCapture: true,
-                action: (evt : TouchEvent) : boolean => {
+                action: (evt: TouchEvent): boolean => {
                     evt.preventDefault();
                     evt.stopPropagation();
-                    // To be completed
+
+                    console.log(getRelevantDataFromEvent(evt));
+
+                    var parentPoint: SVGPoint = transfo.getPoint(getRelevantDataFromEvent(evt).pageX, getRelevantDataFromEvent(evt).pageY);
+
+                    transfo.drag(element, originalMatrix, Pt1_coord_element, parentPoint);
+                    console.log("touchmove done");
+
                     return true;
                 }
             },
-            { from: MT_STATES.Translating,
+            {
+                from: MT_STATES.Translating,
                 to: MT_STATES.Inactive,
                 eventTargets: [document],
                 eventName: ["touchend"],
                 useCapture: true,
-                action: (evt : TouchEvent) : boolean => {
-                    // To be completed
+                action: (evt: TouchEvent): boolean => {
+                    evt.preventDefault();
                     return true;
                 }
             },
-            { from: MT_STATES.Translating, to: MT_STATES.Rotozooming,
+            {
+                from: MT_STATES.Translating, to: MT_STATES.Rotozooming,
                 eventTargets: [element],
                 eventName: ["touchstart"],
                 useCapture: false,
-                action: (evt : TouchEvent) : boolean => {
-                    // To be completed
+                action: (evt: TouchEvent): boolean => {
+                    console.log(getRelevantDataFromEvent(evt));
+
+                    originalMatrix = transfo.getMatrixFromElement(element);
+
+                    // Multiplication of matrix point is done via matrixTransform (wtf?) : https://developer.mozilla.org/en-US/docs/Web/API/SVGPoint
+                    Pt2_coord_element = transfo.getPoint(getRelevantDataFromEvent(evt).pageX, getRelevantDataFromEvent(evt).pageY).matrixTransform(originalMatrix.inverse());
+                    Pt2_coord_parent = transfo.getPoint(getRelevantDataFromEvent(evt).pageX, getRelevantDataFromEvent(evt).pageY);
+
+                    console.log("translating to rotozooming done");
                     return true;
                 }
             },
-            { from: MT_STATES.Rotozooming, to: MT_STATES.Rotozooming,
+            {
+                from: MT_STATES.Rotozooming, to: MT_STATES.Rotozooming,
                 eventTargets: [document],
                 eventName: ["touchmove"],
                 useCapture: true,
-                action: (evt : TouchEvent) : boolean => {
+                action: (evt: TouchEvent): boolean => {
                     evt.preventDefault();
                     evt.stopPropagation();
-                    // To be completed
+
+                    if(getRelevantDataFromEvent(evt).identifier === pointerId_1){
+                          Pt1_coord_parent = transfo.getPoint(getRelevantDataFromEvent(evt).pageX, getRelevantDataFromEvent(evt).pageY);
+                    }
+                    if(getRelevantDataFromEvent(evt).identifier === pointerId_2){
+                          Pt2_coord_parent = transfo.getPoint(getRelevantDataFromEvent(evt).pageX, getRelevantDataFromEvent(evt).pageY);
+                    }
+
+                    transfo.rotozoom(element, originalMatrix, Pt1_coord_element, Pt1_coord_parent, Pt2_coord_element, Pt2_coord_parent);
+
                     return true;
                 }
             },
-            { from: MT_STATES.Rotozooming,
-                to: MT_STATES.Translating,
+            {
+                from: MT_STATES.Rotozooming, to: MT_STATES.Translating,
                 eventTargets: [document],
                 eventName: ["touchend"],
                 useCapture: true,
-                action: (evt : TouchEvent) : boolean => {
-                    const touch = getRelevantDataFromEvent(evt);
-                    // To be completed
+                action: (evt: TouchEvent): boolean => {
+                    originalMatrix = transfo.getMatrixFromElement(element);
+
+                    // Multiplication of matrix point is done via matrixTransform (wtf?) : https://developer.mozilla.org/en-US/docs/Web/API/SVGPoint
+                    Pt1_coord_element = transfo.getPoint(getRelevantDataFromEvent(evt).pageX, getRelevantDataFromEvent(evt).pageY).matrixTransform(originalMatrix.inverse());
+                    Pt1_coord_parent = transfo.getPoint(getRelevantDataFromEvent(evt).pageX, getRelevantDataFromEvent(evt).pageY);
+
                     return true;
                 }
             }
         ]
-    } );
+    });
     fsm.start();
 }
 
 //______________________________________________________________________________________________________________________
 //______________________________________________________________________________________________________________________
 //______________________________________________________________________________________________________________________
-function isString(s : any) : boolean {
-    return typeof(s) === "string" || s instanceof String;
+function isString(s: any): boolean {
+    return typeof (s) === "string" || s instanceof String;
 }
 
-export let $ = (sel : string | Element | Element[]) : void => {
-    let L : Element[] = [];
-    if( isString(sel) ) {
-        L = Array.from( document.querySelectorAll(<string>sel) );
-    } else if(sel instanceof Element) {
-        L.push( sel );
-    } else if(sel instanceof Array) {
+export let $ = (sel: string | Element | Element[]): void => {
+    let L: Element[] = [];
+    if (isString(sel)) {
+        L = Array.from(document.querySelectorAll(<string>sel));
+    } else if (sel instanceof Element) {
+        L.push(sel);
+    } else if (sel instanceof Array) {
         L = sel;
     }
-    L.forEach( multiTouch );
+    L.forEach(multiTouch);
 };
--- ts/transfo.ts
+++ ts/transfo.ts
@@ -1,19 +1,19 @@
 let re_matrix = /^matrix\((.*), (.*), (.*), (.*), (.*), (.*)\)$/;
 
 let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-let idM	= svg.createSVGMatrix();
-idM.a=1; idM.b=0; idM.c=0; idM.d=1; idM.e=0; idM.f=0;
+let idM = svg.createSVGMatrix();
+idM.a = 1; idM.b = 0; idM.c = 0; idM.d = 1; idM.e = 0; idM.f = 0;
 
 //______________________________________________________________________________________________________________________
-export let setMatrixCoordToElement =    ( element: HTMLElement
-                                        , a : number
-                                        , b : number
-                                        , c : number
-                                        , d : number
-                                        , e : number
-                                        , f : number
-                                        ) => {
-    element.style.transform = "matrix(" + a +"," + b +"," + c +"," + d +"," + e +"," + f +")";
+export let setMatrixCoordToElement = (element: HTMLElement
+    , a: number
+    , b: number
+    , c: number
+    , d: number
+    , e: number
+    , f: number
+) => {
+    element.style.transform = "matrix(" + a + "," + b + "," + c + "," + d + "," + e + "," + f + ")";
 };
 
 //______________________________________________________________________________________________________________________
@@ -22,10 +22,10 @@ export let setMatrixToElement = (element: HTMLElement, M: SVGMatrix) => {
 };
 
 //______________________________________________________________________________________________________________________
-export let getMatrixFromString = (str: string) : SVGMatrix => {
-    let res		= re_matrix.exec( str )
-      , matrix	= svg.createSVGMatrix()
-      ;
+export let getMatrixFromString = (str: string): SVGMatrix => {
+    let res = re_matrix.exec(str)
+        , matrix = svg.createSVGMatrix()
+        ;
     matrix.a = parseFloat(res[1]) || 1;
     matrix.b = parseFloat(res[2]) || 0;
     matrix.c = parseFloat(res[3]) || 0;
@@ -36,8 +36,9 @@ export let getMatrixFromString = (str: string) : SVGMatrix => {
     return matrix;
 };
 
+
 //______________________________________________________________________________________________________________________
-export let getPoint = (x: number, y: number) : SVGPoint => {
+export let getPoint = (x: number, y: number): SVGPoint => {
     let point = svg.createSVGPoint();
     point.x = x || 0;
     point.y = y || 0;
@@ -45,27 +46,58 @@ export let getPoint = (x: number, y: number) : SVGPoint => {
 };
 
 //______________________________________________________________________________________________________________________
-export let getMatrixFromElement = (element: Element) : SVGMatrix => {
-	return getMatrixFromString( window.getComputedStyle(element).transform || "matrix(1,1,1,1,1,1)" );
+export let getMatrixFromElement = (element: Element): SVGMatrix => {
+    return getMatrixFromString(window.getComputedStyle(element).transform || "matrix(1,1,1,1,1,1)");
 };
 
 //______________________________________________________________________________________________________________________
-export let drag =       ( element               : HTMLElement
-                        , originalMatrix        : SVGMatrix
-                        , Pt_coord_element      : SVGPoint
-                        , Pt_coord_parent       : SVGPoint
-                        ) => {
-	// TO BE DONE
+export let drag = (element: HTMLElement
+    , originalMatrix: SVGMatrix
+    , Pt_coord_element: SVGPoint
+    , Pt_coord_parent: SVGPoint
+) => {
+    var newMatrix: SVGMatrix = svg.createSVGMatrix();
+    newMatrix.a = originalMatrix.a;
+    newMatrix.b = originalMatrix.b;
+    newMatrix.c = originalMatrix.c;
+    newMatrix.d = originalMatrix.d;
+    newMatrix.e = Pt_coord_parent.x - originalMatrix.a * Pt_coord_element.x - originalMatrix.c * Pt_coord_element.y;
+    newMatrix.f = Pt_coord_parent.y - originalMatrix.b * Pt_coord_element.x - originalMatrix.d * Pt_coord_element.y;
+    setMatrixToElement(element, newMatrix);
 };
 
 //______________________________________________________________________________________________________________________
-export let rotozoom =   ( element           : HTMLElement
-                        , originalMatrix    : SVGMatrix
-                        , Pt1_coord_element : SVGPoint
-                        , Pt1_coord_parent  : SVGPoint
-                        , Pt2_coord_element : SVGPoint
-                        , Pt2_coord_parent  : SVGPoint
-                        ) => {
-	// TO BE DONE
-};
+export let rotozoom = (element: HTMLElement
+    , originalMatrix: SVGMatrix
+    , Pt1_coord_element: SVGPoint
+    , Pt1_coord_parent: SVGPoint
+    , Pt2_coord_element: SVGPoint
+    , Pt2_coord_parent: SVGPoint
+) => {
+    let dxe: number = Pt2_coord_element.x - Pt1_coord_element.x,
+        dye: number = Pt2_coord_element.y - Pt1_coord_element.y,
+        dxp: number = Pt2_coord_parent.x - Pt1_coord_parent.x,
+        dyp: number = Pt2_coord_parent.y - Pt1_coord_parent.y;
+    var newMatrix: SVGMatrix = svg.createSVGMatrix();
+    if (dxe === 0) { //If the points are equals, we give up
+        return;
+    }
+    if (dxe === 0 && dye !== 0) {
+        newMatrix.b = -dxp / dye;
+        newMatrix.a = dyp / dye;
+    }
+    else if (dxe !== 0 && dye === 0) {
+        newMatrix.b = dyp / dxe;
+        newMatrix.a = dxp / dxe;
+    }
+    else if (dxe !== 0 && dye !== 0) {
+        newMatrix.b = (dyp / dye - dxp / dxe) / (dye / dxe + dxe / dye);
+        newMatrix.a = (dyp - newMatrix.b * dxe) / dye;
+    }
+    newMatrix.e = Pt1_coord_parent.x - newMatrix.a * Pt1_coord_element.x + newMatrix.b * Pt1_coord_element.y;
+    newMatrix.f = Pt1_coord_parent.y - newMatrix.b * Pt1_coord_element.x + newMatrix.a * Pt1_coord_element.y;
+    newMatrix.c = -newMatrix.b;
+    newMatrix.d = newMatrix.a;
 
+    setMatrixToElement(element, newMatrix);
+};
